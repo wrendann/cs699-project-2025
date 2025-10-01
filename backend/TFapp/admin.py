54a7f0e9167fcbb8f796831781a204798c967066
Alan Babu <alanaman2000@gmail.com>
2025-10-01 14:27:54 +0000

    added models

--- backend/TFapp/admin.py
+++ backend/TFapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Event, Team, Membership
+
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Team)
+admin.site.register(Membership)
 
 # Register your models here.
