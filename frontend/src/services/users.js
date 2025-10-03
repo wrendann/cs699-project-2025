f8244db5214ebe1b9624364501781a30a2a49189
Alan Babu <amanalan2000@gmail.com>
2025-10-03 22:08:33 +0530

    connecting to backend, login api

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-const baseUrl = "/apiv1/"
+import apiClient from './apiClient';
+const baseUrl = "/api"
 
 export const signInWithEmail = async credentials => {
     if(credentials.credentials.email == 'admin@admin' && credentials.credentials.password == 'testpasswordadmin')
         return {user: "rootuser", token: "rootusertoken"}
     console.log("Sign in attempt with credentials: ", credentials)
-    const response = await axios.post(`${baseUrl}/email`, credentials);
+    const response = await apiClient.post(`/auth/login/`, credentials.credentials);
     return response.data;
 }
 
