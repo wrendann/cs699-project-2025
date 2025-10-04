df2681c3873cd4536cda7f03455f0a13bc85c98c
25M0747 <mziyadr02@gmail.com>
2025-10-04 20:54:26 +0530

    Basic Dashboard build

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,5 +1,68 @@
-const Dashboard = () => {
-    return (<div>Dashboard</div>)
-}
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+import LockerIcon from "../assets/lockerIcon.png";
+import ShipmentsIcon from "../assets/shipmentIcon.png";
+import DeliveredIcon from "../assets/deliveredIcon.png";
+
+
+const WelcomeEventsBox = ({ setLastButton, user }) => {
+  return (
+    <Grid item size={{xs: 12, lg:7}}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        marginLeft={{ xs: "15px", lg: "15px" }}
+        marginRight={{ xs: "15px", lg: "15px" }}
+        sx={{
+          padding: "2.5%",
+          paddingBottom: "3%",
+          borderRadius: "20px",
+          backgroundColor: "white",
+          marginBottom: "15px",
+          marginTop: "20px",
+        }}
+        
+      >
+        <Grid
+          item
+          size={12}
+          style={{
+            backgroundColor: "white",
+            marginBottom: "5px",
+            marginTop: "5px",
+            marginLeft: "10px",
+          }}
+        >
+          <Typography variant="h5" fontFamily="DM Sans" fontWeight="500">
+            Welcome, {user.name}!
+          </Typography>
+        </Grid>
+      </Box>
+
+      <Grid
+        container
+        direction={"row"}
+        spacing={2}
+        style={{ padding: "0px 15px 0px" }}
+        // backgroundColor="red"
+        
+      >
+      </Grid>
+    </Grid>
+  );
+};
+
+const Dashboard = ({ isNarrow, setLastButton, user }) => {
+  return (
+    <Grid container direction={"row"} rowSpacing={1.5}>
+      <WelcomeEventsBox
+        setLastButton={setLastButton}
+        user={user}
+      />
+    </Grid>
+  );
+};
 
 export default Dashboard;
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -3,7 +3,7 @@ const baseUrl = "/apiv1/"
 
 export const signInWithEmail = async credentials => {
     if(credentials.credentials.email == 'admin@admin' && credentials.credentials.password == 'testpasswordadmin')
-        return {user: "rootuser", token: "rootusertoken"}
+        return {user: {name: 'admin', email: 'admin@admin'}, token: "rootusertoken"}
     console.log("Sign in attempt with credentials: ", credentials)
     const response = await axios.post(`${baseUrl}/email`, credentials);
     return response.data;
